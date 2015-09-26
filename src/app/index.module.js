eb52465cd06e38025e99f1428e8adf215e755b81
Marcin Borkowski <marborkowski@gmail.com>
2015-09-26 20:32:14 +0200

    Append 'frames' to the angular's module list.

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -7,11 +7,13 @@ import runBlock from './index.run';
 import MainController from './main/main.controller';
 import NavigationDirective from '../app/components/navigation/navigation.directive';
 import DrawerDirective from '../app/components/drawer/drawer.directive';
+import FramesDirective from '../app/components/frames/frames.directive';
 
 angular.module('animationWizardLed', ['ngAnimate', 'ngSanitize', 'ngResource', 'ngCookies', 'LocalStorageModule', 'ui.router', 'ui.bootstrap'])
   .config(config)
   .config(routerConfig)
   .directive('navigation', () => new NavigationDirective())
   .directive('drawer', () => new DrawerDirective())
+  .directive('frames', () => new FramesDirective())
   .run(runBlock)
   .controller('MainController', MainController);
